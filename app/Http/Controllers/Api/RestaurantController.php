3aad479e33877c9ccceecd30107332c6d627db25
nada rafat <nadarmohamed.97@gmail.com>
2023-04-01 02:27:10 +0200

    prepare the controller for the general data for restaurant with this methods (all restaurants,spasifc restaurant with details)

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use App\Traits\ApiResponses;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    use ApiResponses;
+    public function getAllRestaurants(): JsonResponse
+    {
+        $restaurants = Restaurant::paginate();
+        return $this->responseData(compact('restaurants'),'all restaurants');
+    }
+    public function getRestaurant($id): JsonResponse
+    {
+        $restaurant = Restaurant::find($id);
+        return $this->responseData(compact('restaurant'),'get the restaurant by id');
+    }
+    public function getMealsForRestaurant($restaurant_id): JsonResponse
+    {
+        $restaurant = Restaurant::find($restaurant_id);
+
+        if (!$restaurant) {
+            return $this->responseError(['message' => 'Restaurant not found'], 404);
+        }
+
+        $meals = $restaurant->meals;
+
+        return $this->responseData(compact('meals'),'all meals for this restaurant');
+    }
+    public function getReviewsForRestaurant($restaurant_id): JsonResponse
+    {
+        $restaurant = Restaurant::find($restaurant_id);
+
+        if (!$restaurant) {
+            return $this->responseError(['message' => 'Restaurant not found'], 404);
+        }
+
+        $reviews = $restaurant->reviews;
+
+        return $this->responseData(compact('reviews'),'all reviews for this restaurant');
+    }
+
+}
